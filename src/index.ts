c33cc6145b01b23f1900344e4aac4e2abea411d1
M.S.T.O.P <mstop4@gmail.com>
2023-03-29 13:30:32 -0400

    final touches

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { v4 as uuidV4 } from 'uuid';
 
-type Task = { 
-  id: string,
-  title: string,
-  completed: boolean,
-  createdAt: Date
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
 };
 
 const list = document.querySelector<HTMLUListElement>('#list');
@@ -14,7 +14,7 @@ const tasks: Task[] = loadTasks();
 
 tasks.forEach(addListItem);
 
-form?.addEventListener('submit', e => {
+form?.addEventListener('submit', (e) => {
   e.preventDefault();
 
   if (input?.value == '' || input?.value == null) return;
@@ -23,7 +23,7 @@ form?.addEventListener('submit', e => {
     id: uuidV4(),
     title: input.value,
     completed: false,
-    createdAt: new Date()
+    createdAt: new Date(),
   };
   tasks.push(newTask);
 
